73066e3d6e4f47045da96b1ef65c5beba6e9f292
Glaaki <glaaki@protonmail.com>
2018-03-24 17:38:54 +0100

    confirmation before reset

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root 'articles#index'
 
-  get 'destroy_all', to: 'articles#destroy_all'
+  delete 'destroy_all', to: 'articles#destroy_all'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
