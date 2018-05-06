9ff351a14bdfd6d05da2daa54fb3401bf8eed2ef
Glaaki <glaaki@protonmail.com>
2018-05-06 13:08:11 +0200

    refactored listes pages

--- app/helpers/listes_helper.rb
+++ app/helpers/listes_helper.rb
@@ -1,2 +1,19 @@
 module ListesHelper
+
+  def listes_user_actif(all_listes)
+    all_listes.select{|l| l.user_id == session[:user_id]}
+  end
+
+  def owns_listes?(all_listes)
+    listes_user_actif(all_listes).size > 0
+  end
+
+  def listes_partagees(all_listes)
+    all_listes.select{|l| l.user_id != session[:user_id]}
+  end
+
+  def receives_listes?(all_listes)
+    listes_partagees(all_listes).size > 0
+  end
+
 end
